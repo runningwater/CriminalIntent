7d7df31292ef20f4f94cebf68209311fb3cc4b3f
runningwater <ynwdlxm@163.com>
2016-11-01 18:02:14 +0800

    添加 日期 fragment

--- app/src/main/java/com/android/elabcare/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/android/elabcare/criminalintent/CrimeFragment.java
@@ -1,11 +1,15 @@
 package com.android.elabcare.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +17,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import com.android.elabcare.criminalintent.dialog.DatePickerFragment;
 
 import java.util.Date;
 import java.util.UUID;
@@ -22,8 +28,15 @@ import java.util.UUID;
  */
 // CrimeFragment 类是与模型及视图对象交互的控制器
 public class CrimeFragment extends Fragment {
+    private static final String TAG = "CrimeFragment";
+    private static final boolean DEBUG = true;
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
+
+    private static final int RQUEUEST_DATE = 0;
+
+    private static String DateFormatPattern = "yyy年MM月dd日hh时mm分";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -72,9 +85,19 @@ public class CrimeFragment extends Fragment {
             }
         });
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        Date date = mCrime.getDate();
-        mDateButton.setText(DateFormat.format("yyy年MM月dd日", date).toString());
-        mDateButton.setEnabled(false);
+        final Date date = mCrime.getDate();
+        mDateButton.setText(DateFormat.format(DateFormatPattern, date).toString());
+        //mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                //DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(date);
+                dialog.setTargetFragment(CrimeFragment.this, RQUEUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -84,5 +107,18 @@ public class CrimeFragment extends Fragment {
             }
         });
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (DEBUG) Log.d(TAG, "返回CrimeFragment时调用方法.........onActivityResult");
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == RQUEUEST_DATE) {
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            mDateButton.setText(DateFormat.format(DateFormatPattern, mCrime.getDate()).toString());
+        }
     }
 }
--- app/src/main/java/com/android/elabcare/criminalintent/CrimePageActivity.java
+++ app/src/main/java/com/android/elabcare/criminalintent/CrimePageActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +19,8 @@ import java.util.UUID;
  */
 
 public class CrimePageActivity extends FragmentActivity {
+    private static final String TAG = "CrimePageActivity";
+    private static final boolean DEBUG = true;
     private static final String EXTRA_CRIME_ID = "com.android.elabcare.criminalintent.crime_id";
 
 
@@ -36,6 +39,7 @@ public class CrimePageActivity extends FragmentActivity {
         setContentView(R.layout.activity_crime_pager);
 
         UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        if (DEBUG) Log.i(TAG, "crimeId=" + crimeId);
 
         mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
